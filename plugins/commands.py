6f975b1a1f0060fd84df5099f4f52f1bed786679
1hari11111111 <uhvardhan987@gmail.com>
2024-07-29 06:40:59 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -586,7 +586,7 @@ async def start(client, message):
             pass
         return await message.reply('No such file exist.')
     files = files_[0]
-    title = '@namaste_cineverse ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
+    title = '@Team_mwtel ' + ' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@'), files.file_name.split()))
     size=get_size(files.file_size)
     f_caption=files.caption
     # if CUSTOM_FILE_CAPTION:
@@ -596,7 +596,7 @@ async def start(client, message):
     #         logger.exception(e)
     #         #TODO REMOVED THIS LINE f_caption=f_caption
     if f_caption is None:
-        f_caption = f"@namaste_cineverse {file.file_name.replace('cineverse','')}"
+        f_caption = f"@Team_mwtel {file.file_name.replace('cineverse','')}"
     if not await db.has_premium_access(message.from_user.id):
         if not await check_verification(client, message.from_user.id) and VERIFY == True:
             btn = [[
